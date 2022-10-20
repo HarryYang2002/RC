78b99b6d34c0fe4aee28047ef1eeb08bd26eac31
HarryYang2002 <1260828355@qq.com>
2022-10-20 22:17:07 +0800

    身份服务的定义与实现（bug）

--- miniprogram/pages/register/register.ts
+++ miniprogram/pages/register/register.ts
@@ -1,15 +1,27 @@
+import { ProfileService } from "../../service/profile"
+import { rental } from "../../service/proto_gen/rental/rental_pb"
+import { padString } from "../../utils/format"
 import { routing } from "../../utils/routing"
+
+function formatDate(millis: number) {
+	const dt = new Date(millis)
+	const y = dt.getFullYear()
+	const m = dt.getMonth() + 1
+	const d = dt.getDate()
+	return `${padString(y)}-${padString(m)}-${padString(d)}`
+}
 
 Page({
 	redirectURL: '',
+	profileRefresher: 0,
 	data: {
-		licImgURL: '',
-		genders: ['未知', '男', '女', '其他'],
-		genderIndex: 0,
-		birthDate: '1970-01-01',
 		licNo: '',
 		name: '',
-		state: 'UNSUBMITTED' as 'UNSUBMITTED' | 'PENDING' | 'VERIFIED',
+		genderIndex: 0,
+		genders: ['未知', '男', '女'],
+		birthDate: '1990-01-01',
+		licImgURL: '',
+		state: rental.v1.IdentityStatus[rental.v1.IdentityStatus.UNSUBMITTED],
 	},
 	onUnloadLic() {
 		wx.chooseMedia({
@@ -31,46 +43,82 @@ Page({
 		})
 	},
 
-	onLoad(opt: Record<"redirect",string>) {
-		const o : routing.RegisterOpts = opt
+	renderProfile(p: rental.v1.IProfile) {
+		this.setData({
+			licNo: p.identity?.licNumber || "",
+			name: p.identity?.name || "",
+			genderIndex: p.identity?.gender || 0,
+			birthDate: formatDate(p.identity?.birthDateMillis || 0),
+			state: rental.v1.IdentityStatus[p.identityStatus || 0],
+		})
+	},
+
+	onLoad(opt: Record<"redirect", string>) {
+		const o: routing.RegisterOpts = opt
 		if (o.redirect) {
 			this.redirectURL = decodeURIComponent(o.redirect)
 		}
+		ProfileService.getProfile().then(p => this.renderProfile(p))
 	},
 
 	onGenderChange(e: any) {
 		this.setData({
-			genderIndex: e.detail.value,
+			genderIndex: parseInt(e.detail.value),
 		})
 	},
+	
 	onBirthDateChange(e: any) {
 		this.setData({
 			birthDate: e.detail.value,
 		})
 	},
-	onSubmit() {
-		this.setData({
-			state: 'PENDING',
-		})
-		setTimeout(() => {
-			this.onLicVerified()
-		}, 3000)
-	},
-	onResubmit() {
-		this.setData({
-			state: 'UNSUBMITTED',
-			licImgURL: '',
-		})
-	},
-	onLicVerified() {
-		this.setData({
-			state: 'VERIFIED',
-		})
-		if (this.redirectURL) {
-			wx.redirectTo({
-				url: this.redirectURL,
-			})
-		}
 
-	}
+	onSubmit() {
+		ProfileService.submitProfile({
+            licNumber: this.data.licNo,
+            name: this.data.name,
+            gender: this.data.genderIndex,
+            birthDateMillis: Date.parse(this.data.birthDate),
+        }).then(p => {
+            this.renderProfile(p)
+            this.scheduleProfileRefresher()
+        })
+	},
+
+	onUnload() {
+        this.clearProfileRefresher()
+    },
+
+	scheduleProfileRefresher() {
+        this.profileRefresher = setInterval(() => {
+            ProfileService.getProfile().then(p => {
+                this.renderProfile(p)
+                if (p.identityStatus !== rental.v1.IdentityStatus.PENDING) {
+                    this.clearProfileRefresher()
+                }
+                if (p.identityStatus === rental.v1.IdentityStatus.VERIFIED) {
+                    this.onLicVerified()
+                }
+            })
+        }, 1000)
+    },
+
+	clearProfileRefresher() {
+        if (this.profileRefresher) {
+            clearInterval(this.profileRefresher)
+            this.profileRefresher = 0
+        }
+    },
+
+	onResubmit() {
+		ProfileService.clearProfile().then(p => this.renderProfile(p))
+	},
+
+	onLicVerified() {
+        if (this.redirectURL) {
+            wx.redirectTo({
+                url: this.redirectURL,
+            })
+        }
+    }
 })
